d2bb9e11f8a82e4ba32c3fa4156a5a3327472a60
Vinitha Bolle <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-21 02:17:41 +0530

    Add files via upload

---
+++ conditions used in sakila.inventory database.py
@@ -0,0 +1,5 @@
+select * from sakila.inventory;
+select * from sakila.inventory where film_id > 1;
+select * from sakila.inventory where store_id > 1;
+select * from sakila.inventory where inventory_id = 5;
+select * from sakila.inventory where inventory_id > 5;
