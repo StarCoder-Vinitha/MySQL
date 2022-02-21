c13ef921808f211fb88b985dc661d5580ee8e5e7
Vinitha Bolle <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-22 04:36:20 +0530

    Add files via upload

---
+++ Maximum of all columns from sakila.payment database.sql
@@ -0,0 +1,7 @@
+select * from sakila.payment;
+select max(payment_id) from sakila.payment;
+select max(customer_id) from sakila.payment;
+select max(staff_id) from sakila.payment;
+select max(amount) from sakila.payment;
+select max(payment_date) from sakila.payment;
+select max(last_update) from sakila.payment;
