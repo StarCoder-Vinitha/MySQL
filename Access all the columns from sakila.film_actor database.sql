dadcec514414e140c47835e68e8a0aa20d5197fc
Vinitha Bolle <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-23 22:38:37 +0530

    Add files via upload

---
+++ Access all the columns from sakila.film_actor database.sql
@@ -0,0 +1,6 @@
+select * from sakila.film_actor;
+select actor_id from sakila.film_actor;
+select film_id from sakila.film_actor;
+select last_update from sakila.film_actor;
+select actor_id = 7 from sakila.film_actor;
+select film_id = 1 from sakila.film_actor;
