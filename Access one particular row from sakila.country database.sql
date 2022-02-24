4eedfade58e5eac740cf015c5943bbbc2c54b900
Vinitha Bolle <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-24 21:31:33 +0530

    Add files via upload
    Accessing only one single particular row from sakila.country database.

---
+++ Access one particular row from sakila.country database.sql
@@ -0,0 +1,4 @@
+select * from sakila.country;
+select * from sakila.country where country_id = 5;
+select * from sakila.country where country = Afghanistan;
+select * from sakila.country where last_update = 2006-02-15 04:44:00;
