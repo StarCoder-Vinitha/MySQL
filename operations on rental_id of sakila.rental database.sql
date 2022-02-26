05e03587dcd57dc1be3ded2df6700901552699b6
Vinitha Bolle <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-26 11:32:30 +0530

    Add files via upload
    sum, avg, min, max operations performed on rental_id column in rental table from sakila database

---
+++ operations on rental_id of sakila.rental database.sql
@@ -0,0 +1,6 @@
+select * from sakila.rental;
+select sum(rental_id) from sakila.rental;
+select avg(rental_id) from sakila.rental;
+select min(rental_id) from sakila.rental;
+select max(rental_id) from sakila.rental;
+select (rental_id) from sakila.rental;
