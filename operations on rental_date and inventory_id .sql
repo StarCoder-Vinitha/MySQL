17737ee7ba6889805dbbaefeb5da7a65c80cd1c6
Vinitha Bolle <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-02-26 11:44:40 +0530

    Add files via upload
    sum(), avg(), min(). max() operations performed on rental_date and inventory_id columns in rental table from sakila database

---
+++ operations on rental_date and inventory_id .sql
@@ -0,0 +1,10 @@
+select * from sakila.rental;
+select sum(rental_date) from sakila.rental;
+select avg(rental_date) from sakila.rental;
+select min(rental_date) from sakila.rental;
+select max(rental_date) from sakila.rental;
+-- 
+select sum(inventory_id) from sakila.rental;
+select avg(inventory_id) from sakila.rental;
+select min(inventory_id) from sakila.rental;
+select max(inventory_id) from sakila.rental;
