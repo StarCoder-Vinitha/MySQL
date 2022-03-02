1f9fe93a03ed9304d4ef5afc1c44275eabb418eb
Vinitha Bolle <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-03-02 18:07:07 +0530

    Add files via upload
    Applying built in functions on test table of class database

---
+++ built in functions on class.test database.sql
@@ -0,0 +1,7 @@
+select * from class.test;
+/* Built in functions in mySQL...*/
+# string functions in mySQL
+select upper(name) from test;
+select * from test
+select * from test group by id
+select * from test order by marks
