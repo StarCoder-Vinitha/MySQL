6b0f862e4fb161d7f07c6bde14b7b349e4617b02
Vinitha Bolle <99254881+StarCoder-Vinitha@users.noreply.github.com>
2022-03-02 17:56:19 +0530

    Add files via upload
    create one database naming of class

---
+++ creating class database.sql
@@ -0,0 +1,14 @@
+create database class;
+use class;
+create table test(id int, name varchar(20), marks int, class int);
+insert into test values (1, 'Vinitha', 95, 10);
+insert into test values (2, 'Anand', 85, 10);
+insert into test values (3, 'Pandu', 80, 10);
+insert into test values (4, 'Raj', 75, 10);
+insert into test values (5, 'Prashu', 50, 10);
+insert into test values (6, 'Honey', 70, 10);
+insert into test values (7, 'Chandu', 78, 10);
+insert into test values (8, 'Ammu', 75, 10);
+insert into test values (9, 'Ammu', 75, 10);
+
+select * from class.test;
